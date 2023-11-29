85537cefd13d4c370eb71aa2eb9ce7ddb5360aa2
Keiran <keirankozlowski@gmail.com>
2023-11-29 12:29:57 -0500

    Updated apiurl

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const COHORT = "REPLACE_ME!";
-const API_URL = `https://fsa-async-await.herokuapp.com/api/${COHORT}/artists`;
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/artists`;
 
 const state = {
   artists: [],
